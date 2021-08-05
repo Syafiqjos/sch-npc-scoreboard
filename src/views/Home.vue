12fa94b307910e2f20a529a11db26b2d309903d8
Syafiqjos <syafiqjos@gmail.com>
2021-08-06 01:14:07 +0700

    Fix Scoreboard Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="center-absolute">
+      <img class="center-block" alt="Schematics logo" src="https://schematics.its.ac.id/favicon/apple-icon-180x180.png" />
+      <h1 class="center-block" align="center" >Schematics NPC 2021</h1>
+      <v-btn style="width:100%;" class="center-block" to="./scoreboard">Scoreboard</v-btn>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style>
+.center-absolute {
+  position:absolute;
+  left:50%;
+  top:50%;
+  transform: translate(-50%, -50%);
+}
+.center-block {
+  margin: auto;
+  display:block;
+}
+.center-block {
+  margin: auto;
+}
+</style>
