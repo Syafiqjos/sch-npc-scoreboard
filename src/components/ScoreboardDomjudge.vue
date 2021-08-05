84483cafe91c29d79401324fc860a899aa601dd4
Syafiqjos <syafiqjos@gmail.com>
2021-08-05 23:47:33 +0700

    Change hardcoded images data into loadable json

--- src/components/ScoreboardDomjudge.vue
+++ src/components/ScoreboardDomjudge.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-simple-table v-if="scoreboard_data && problems && rankings && teams && organizations" class="scoreboard" fixed-header>
+    <v-simple-table v-if="scoreboard_data && problems && rankings && teams && organizations && organizations_images" class="scoreboard" fixed-header>
         <template v-slot:default>
             <thead>
                 <tr>
@@ -64,31 +64,7 @@ export default {
             rankings : null,
             teams : null,
             organizations : null,
-            organizations_images : {
-                'its' : { 'name' : 'Institut Teknologi Sepuluh Nopember', 'image' : '/institute_images/its.png' } ,
-                'unair' : { 'name' : 'Universitas Airlangga', 'image' : 'unair.png' } ,
-                'itb' : { 'name' : 'Institut Teknologi Bandung', 'image' : 'itb.png' } ,
-                'ui' : { 'name' : 'Universitars Indonesia', 'image' : 'ui.png' } ,
-                'unsyiah' : { 'name' : 'Universitas Syiah Kuala', 'image' : 'unsyiah.png' } ,
-                'unimal' : { 'name' : 'Universitas Malikussaleh', 'image' : 'unimal.png' } ,
-                'unsam' : { 'name' : 'Universitas Samudra', 'image' : 'unsam.png' } ,
-                'utu' : { 'name' : 'Universitas Teuku Umar', 'image' : 'utu.png' } ,
-                'uinar' : { 'name' : 'UIN Ar-Raniry', 'image' : 'uinar.png' } ,
-                'iain_langsa' : { 'name' : 'IAIN Langsa', 'image' : 'iain_langsa.png' } ,
-                'poltekkes_aceh' : { 'name' : 'Politeknik Kesehatan Banda Aceh', 'image' : 'poltekkes_aceh.png' } ,
-                'usm' : { 'name' : 'Universitas Serambi Mekkah', 'image' : 'usm.png' } ,
-                'usu' : { 'name' : 'Universitas Sumatera Utara', 'image' : 'usu.png' } ,
-                'unimed' : { 'name' : 'Universitas Negeri Medan', 'image' : 'unimed.png' } ,
-                'polmed' : { 'name' : 'Politeknik Negeri Medan', 'image' : 'polmed.png' } ,
-                'polimedia' : { 'name' : 'Politeknik Negeri Media Kreatif', 'image' : 'polimedia.png' } ,
-                'uinsu' : { 'name' : 'UIN Sumatera Utara', 'image' : 'uinsu.png' } ,
-                'iain_padangsidimpuan' : { 'name' : 'IAIN Padang Sidempuan', 'image' : 'iain_padangsidimpuan.png' } ,
-                'unand' : { 'name' : 'Universitas Andalas', 'image' : 'unand.png' } ,
-                'unp' : { 'name' : 'Universitas Negeri Padang', 'image' : 'unp.png' } ,
-                'pnp' : { 'name' : 'Politeknik Negeri Padang', 'image' : 'pnp.png' } ,
-                'unri' : { 'name' : 'Universitas Riau', 'image' : 'UNRI.png' } ,
-                'UGM' : { 'name' : 'Universitas Gadjah Mada', 'image' : 'UGM.png' }
-            }
+            organizations_images : null
         }
     },
     props : [
@@ -102,6 +78,7 @@ export default {
             this.populateTable();
             this.retrieveTeams();
             this.retrieveOrganizations();
+            this.retrieveOrganizationImages();
         },
         populateTable(){
             console.log("PopulateTableAsDomjudge");
@@ -149,8 +126,17 @@ export default {
 
                 this.organizations = organizations_new;
                 console.log(this.organizations);
+            })
+            .catch((errors) => {
+                console.log(errors);
+            });
+        },
+        retrieveOrganizationImages(){
+            this.axios.get("/scoreboard_data/institute_images.json")
+            .then((response) => {
+                this.organizations_images = response.data;
 
-                this.show_scoreboard = true;
+                console.log(this.organizations_images);
             })
             .catch((errors) => {
                 console.log(errors);
