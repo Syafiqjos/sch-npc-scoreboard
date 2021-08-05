69e184c0dbcf90cde126affd401bcd8254ef11f3
Syafiqjos <syafiqjos@gmail.com>
2021-08-06 01:23:49 +0700

    Change Home views into component and slot

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="center-absolute">
+      <img class="center-block" alt="Schematics logo" src="https://schematics.its.ac.id/favicon/apple-icon-180x180.png" />
+      <h1 class="center-block" align="center" >Schematics NPC 2021</h1>
+      <slot name="content"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent'
+}
+</script>
+
+<style>
+.center-absolute {
+  position:absolute;
+  left:50%;
+  top:50%;
+  transform: translate(-50%, -50%);
+}
+.center-block {
+  margin: auto;
+  display:block;
+}
+.center-block {
+  margin: auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,19 @@
 <template>
-    <div class="center-absolute">
-      <img class="center-block" alt="Schematics logo" src="https://schematics.its.ac.id/favicon/apple-icon-180x180.png" />
-      <h1 class="center-block" align="center" >Schematics NPC 2021</h1>
-      <v-btn style="width:100%;" class="center-block" to="./scoreboard">Scoreboard</v-btn>
-    </div>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HomeComponent>
+      <template v-slot:content>
+        <v-btn style="width:100%;" class="center-block" to="./scoreboard">Scoreboard</v-btn>
+      </template>
+    </HomeComponent>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import HomeComponent from '@/components/HomeComponent.vue'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components : {
+    HomeComponent
+  }
 }
 </script>
-
-<style>
-.center-absolute {
-  position:absolute;
-  left:50%;
-  top:50%;
-  transform: translate(-50%, -50%);
-}
-.center-block {
-  margin: auto;
-  display:block;
-}
-.center-block {
-  margin: auto;
-}
-</style>
