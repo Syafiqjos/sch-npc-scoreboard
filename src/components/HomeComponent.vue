e48c29e613f171816005e3056055296761627f58
Syafiqjos <syafiqjos@gmail.com>
2021-09-23 13:00:02 +0700

    Perfecting config json and fix homepage logo and sponsor component margin

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,6 +1,6 @@
 <template v-if="$parent.app_config.active">
     <div v-if="$parent.app_config" class="center-relative home-component">
-      <img class="center-block" alt="Schematics logo" :src="$parent.app_config.homepage_logo_url" />
+      <img class="center-block" style="margin-top: 20px;" alt="Schematics logo" :src="$parent.app_config.homepage_logo_url" />
       <h1 class="center-block" align="center" >{{ $parent.app_config.event_display_name }}</h1>
       <slot name="content"></slot>
       <SponsorComponent style="margin-left: 2rem; margin-right: 2rem;" v-if="$parent.app_config.sponsor && $parent.app_config.sponsor.active" :app_config="$parent.app_config"></SponsorComponent>
