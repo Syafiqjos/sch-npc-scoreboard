10c196fb805956ea08d96fd5fbebbbfa769faa27
Syafiqjos <syafiqjos@gmail.com>
2021-09-23 12:54:01 +0700

    Make sponsor component responsive

--- src/components/SponsorComponent.vue
+++ src/components/SponsorComponent.vue
@@ -2,7 +2,7 @@
     <div class="sponsor-container">
         <div class="sponsor-title">{{app_config.sponsor.sponsor_text}}</div>
         <div><img class="sponsor-banner" :src="app_config.sponsor.sponsor_banner_image_url" alt="" /></div>
-        <v-layout align-center justify-center>
+        <v-layout class="sponsor-list">
             <img v-for="(sponsor, index) in app_config.sponsor.sponsors" :key="'sponsor-' + index"
              :style="sponsor.height == '' ? '' : ('height: ' + sponsor.height + ';')" class="sponsor-img" :src="sponsor.image_url" :alt="sponsor.image_alt" />
         </v-layout>
